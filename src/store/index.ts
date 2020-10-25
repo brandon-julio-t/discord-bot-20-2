04564041b70e6efab5c785bf8153210791aab0fc
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2020-10-25 13:20:32 +0700

    remove unused imports

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,8 @@
 import SpecialShift from '../models/special-shift';
 import WorkingShift from '../models/working-shift';
-import { Client, NewsChannel, DMChannel, TextChannel } from 'discord.js';
-import { ScheduledTask } from 'node-cron';
+import { Client } from 'discord.js';
 import { DiscordChannel } from '../core/type-aliases';
+import { ScheduledTask } from 'node-cron';
 
 interface State {
   assistantsSpecialShifts: SpecialShift[];
